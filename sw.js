fe639cba03d7b9607efb188caa9023b536b35c0e
xfifafo <djpana@live.com>
2025-05-01 19:26:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,67 @@
-const CACHE_NAME = 'bcn-latina-v1';
-const urlsToCache = [
+const CACHE_NAME = 'bcn-latina-radio-v1';
+const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
-  '/styles.css',
+  '/manifest.json',
+  '/sw.js',
   '/img/logo.png',
-  '/img/whatsapp-icon.png',
-  '/img/instagram-icon.png',
-  '/img/facebook-icon.png',
-  '/img/tiktok-icon.png',
-  '/img/youtube-icon.png',
-  '/video/video.mp4'
+  'https://stream.zeno.fm/2kd6wvz7zxhvv'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+      .catch((error) => {
+        console.error('Error during service worker installation:', error);
+      })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => {
+        return self.clients.claim();
       })
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then((response) => {
+            if (!response || response.status !== 200) {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          })
+          .catch((error) => {
+            console.error('Fetch error:', error);
+            throw error;
+          });
+      })
   );
 });
