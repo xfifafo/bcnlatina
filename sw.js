8bee35010803864fad04a820c88718d8f623c4a8
xfifafo <djpana@live.com>
2025-05-04 17:33:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
 const CACHE_NAME = 'bcn-latina-radio-v1';
 const ASSETS_TO_CACHE = [
-  '.',
-  './index.html',
-  './manifest.json',
-  './sw.js',
-  './favicon.ico',
-  './img/logo.png',
-  'https://stream.zeno.fm/2kd6wvz7zxhvv'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/sw.js',
+  '/favicon.ico',
+  '/img/logo.png'
 ];
 
 self.addEventListener('install', (event) => {
@@ -21,15 +20,13 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((cacheName) => {
-            if (cacheName !== CACHE_NAME) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
+      .then((cacheNames) => Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      ))
       .then(() => self.clients.claim())
   );
 });
@@ -37,22 +34,14 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
+      .then((response) => response || fetch(event.request).then((fetchResponse) => {
+        if (!fetchResponse || fetchResponse.status !== 200) {
+          return fetchResponse;
         }
-        return fetch(event.request)
-          .then((response) => {
-            if (!response || response.status !== 200) {
-              return response;
-            }
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          });
-      })
+        const responseToCache = fetchResponse.clone();
+        caches.open(CACHE_NAME)
+          .then((cache) => cache.put(event.request, responseToCache));
+        return fetchResponse;
+      }))
   );
 });
